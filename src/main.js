6407ea5798ffc99e0555a65d7eafc88491ce4cb8
Alan <pa013971@gmail.com>
2021-11-09 01:37:48 +0800

    Add - 新增 axios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import './plugins/bootstrap-vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  VueAxios,
+  axios,
   render: h => h(App)
 }).$mount('#app')
