918e6a5fe5eae09506d97503e2a8ae08a640dd2e
Alan <pa013971@gmail.com>
2021-11-09 04:43:46 +0800

    Add - 加入 Card (未完成)

--- src/views/Week01.vue
+++ src/views/Week01.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <!-- card -->
+      <Card :itemsData="itemsData" />
+    <!-- card -->
+
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import Card from '@/components/Card.vue'
+export default {
+  data() {
+    return {
+      type: 0,
+      name: ["ScenicSpot", "Restaurant", "Hotel", "Activity"],
+      itemsData: [],
+    }
+  },
+  mounted () {
+    this.getItem(this.name[this.type]);
+  },
+  methods: {
+    getItem(name) {
+      console.log(`${name}`);
+      let api_url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism';
+      axios({
+        methods: 'get',
+        url: `${api_url}/${name}`,
+        params: {
+          // "$select": "name",
+          "$filter": "Picture/PictureUrl1 ne null",
+          "$top": 5,
+        }
+      })
+      .then((resp) => {
+        console.log(resp.data);
+        this.itemsData = resp.data
+      });
+
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
