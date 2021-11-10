af9341a403a98c4c133d5a6182956027db44fe08
Alan <alan@advmeds.com>
2021-11-10 18:43:11 +0800

    Add - 新增導覽圖按鈕

--- src/views/Week01.vue
+++ src/views/Week01.vue
@@ -6,7 +6,7 @@
       id="carousel-fade"
       style="text-shadow: 0px 0px 2px #000"
       fade
-      :interval=50000
+      :interval=500000
     >
       <b-carousel-slide
         img-src="../assets/week01/01-background.png"
@@ -20,6 +20,14 @@
       <b-carousel-slide
         img-src="../assets/week01/04-background.png"
       ></b-carousel-slide>
+
+        <div class="carousel-content">
+          <h1 class="mx-3">景點</h1>
+          <b-button class="mx-1 px-3"  pill variant="danger">景點<b-icon class="ml-2" icon="camera-fill"></b-icon></b-button>
+          <b-button class="mx-1 px-3"  pill variant="light">餐飲<b-icon class="ml-2" icon="geo-alt-fill"></b-icon></b-button>
+          <b-button class="mx-1 px-3"  pill variant="light">旅宿<b-icon class="ml-2" icon="house-fill"></b-icon></b-button>
+          <b-button class="mx-1 px-3"  pill variant="light">活動<b-icon class="ml-2" icon="geo-alt-fill"></b-icon></b-button>
+        </div>
     </b-carousel>
     <!-- carousel -->
 
@@ -73,5 +81,28 @@ export default {
 <style scoped>
 .week01 {
   min-height: 50rem;
+}
+
+.carousel-content {
+  position: absolute;
+  z-index: 1;
+  left: 10rem;
+  top: 5rem;
+}
+
+.carousel-content h1{
+  font: var(--unnamed-font-style-normal) normal bold 80px/106px var(--unnamed-font-family-roboto);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-ffffff);
+  text-align: left;
+  font: normal normal bold 80px/106px Roboto;
+  color: #FFFFFF;
+  text-shadow: 0px 3px 6px #00000029;
+  line-height: 80px;
+}
+
+.carousel-content svg {
+  font-size: 15px;
+  margin-bottom: 5px;
 }
 </style>
